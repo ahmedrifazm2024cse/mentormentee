fb74838a347137cb83d4f86ad18d01127fbcfc83
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-10-26 17:51:58 +0530

    updated allocation repository

--- mentorship-portal/src/main/java/com/mentorshipportal/repository/AllocationRepository.java
+++ mentorship-portal/src/main/java/com/mentorshipportal/repository/AllocationRepository.java
@@ -1,4 +1,14 @@
 package com.mentorshipportal.repository;
 
-public class AllocationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mentorshipportal.model.Allocation;
+import java.util.List;
+
+public interface AllocationRepository extends JpaRepository<Allocation, Long> {
+
+    // Find all mentees for a specific mentor
+    List<Allocation> findByMentor_MentorId(Long mentorId);
+
+    // Find mentor for a specific mentee
+    List<Allocation> findByMentee_MenteeId(Long menteeId);
 }
