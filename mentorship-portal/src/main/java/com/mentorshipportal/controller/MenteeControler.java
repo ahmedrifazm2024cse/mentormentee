9f909d1adcd21e31f1b3457a3460bd034e581003
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-10-26 18:10:35 +0530

    updated MenteeController java class

--- mentorship-portal/src/main/java/com/mentorshipportal/controller/MenteeControler.java
+++ mentorship-portal/src/main/java/com/mentorshipportal/controller/MenteeControler.java
@@ -1,4 +1,30 @@
 package com.mentorshipportal.controller;
 
-public class MenteeControler {
+import com.mentorshipportal.model.Mentee;
+import com.mentorshipportal.service.MenteeService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mentees")
+@CrossOrigin("*")
+public class MenteeController {
+
+    private final MenteeService menteeService;
+
+    public MenteeController(MenteeService menteeService) {
+        this.menteeService = menteeService;
+    }
+
+    // Add mentee
+    @PostMapping
+    public Mentee addMentee(@RequestBody Mentee mentee) {
+        return menteeService.addMentee(mentee);
+    }
+
+    // View all mentees
+    @GetMapping
+    public List<Mentee> getAllMentees() {
+        return menteeService.getAllMentees();
+    }
 }
