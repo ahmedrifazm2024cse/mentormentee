c56be89b32335071d33800199a2ac5983947c447
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-10-26 18:09:42 +0530

    updated mentorcontroller java class

--- mentorship-portal/src/main/java/com/mentorshipportal/controller/MentorController.java
+++ mentorship-portal/src/main/java/com/mentorshipportal/controller/MentorController.java
@@ -1,4 +1,30 @@
 package com.mentorshipportal.controller;
 
+import com.mentorshipportal.model.Mentor;
+import com.mentorshipportal.service.MentorService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/mentors")
+@CrossOrigin("*") // allows frontend apps to access this API
 public class MentorController {
+
+    private final MentorService mentorService;
+
+    public MentorController(MentorService mentorService) {
+        this.mentorService = mentorService;
+    }
+
+    // Add mentor
+    @PostMapping
+    public Mentor addMentor(@RequestBody Mentor mentor) {
+        return mentorService.addMentor(mentor);
+    }
+
+    // View all mentors
+    @GetMapping
+    public List<Mentor> getAllMentors() {
+        return mentorService.getAllMentors();
+    }
 }
