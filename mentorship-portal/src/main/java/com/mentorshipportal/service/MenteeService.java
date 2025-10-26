91e1432db34f699ef788437425f890624723fb24
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-10-26 18:02:58 +0530

    updated MenteeService java class

--- mentorship-portal/src/main/java/com/mentorshipportal/service/MenteeService.java
+++ mentorship-portal/src/main/java/com/mentorshipportal/service/MenteeService.java
@@ -1,4 +1,24 @@
 package com.mentorshipportal.service;
 
+import com.mentorshipportal.model.Mentee;
+import com.mentorshipportal.repository.MenteeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class MenteeService {
+
+    private final MenteeRepository menteeRepository;
+
+    public MenteeService(MenteeRepository menteeRepository) {
+        this.menteeRepository = menteeRepository;
+    }
+
+    public Mentee addMentee(Mentee mentee) {
+        return menteeRepository.save(mentee);
+    }
+
+    public List<Mentee> getAllMentees() {
+        return menteeRepository.findAll();
+    }
 }
