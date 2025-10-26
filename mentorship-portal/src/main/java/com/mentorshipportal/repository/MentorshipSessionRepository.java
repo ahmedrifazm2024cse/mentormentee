c07fd832ad63482035131c59c5cda9a47f4fce7e
ahmedrifazm2024cse <ahmedrifaz.m2024cse@sece.ac.in>
2025-10-26 17:56:07 +0530

    updated Mentorshipportal

--- mentorship-portal/src/main/java/com/mentorshipportal/repository/MentorshipSessionRepository.java
+++ mentorship-portal/src/main/java/com/mentorshipportal/repository/MentorshipSessionRepository.java
@@ -1,4 +1,11 @@
 package com.mentorshipportal.repository;
 
-public class MentorshipSessionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mentorshipportal.model.MentorshipSession;
+import java.util.List;
+
+public interface MentorshipSessionRepository extends JpaRepository<MentorshipSession, Long> {
+
+    // Find all sessions for an allocation
+    List<MentorshipSession> findByAllocation_AllocationId(Long allocationId);
 }
